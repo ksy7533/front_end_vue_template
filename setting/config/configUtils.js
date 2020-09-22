dc8c82550b6dde7e58abf05fdad7b6928c60db9d
ksy7533 <ksy7533@gmail.com>
2020-09-22 16:19:16 +0900

    cross-env 모듈을 사용하여 모드별 환경설정 처리가능하도록 한다

--- setting/config/configUtils.js
+++ setting/config/configUtils.js
@@ -1,4 +1,7 @@
 const merge = require("deepmerge");
+
+console.log(process.env.FRONT_MODE);
+
 module.exports = {
   modeConfigurer: function(projectConfigurer) {
     return merge({}, projectConfigurer);
